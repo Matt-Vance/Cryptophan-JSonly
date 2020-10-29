bfce83b902c65275f8c7025b898854a6116838d2
Matt Vance <mvance43776@gmail.com>
2020-10-28 23:18:03 -0700

    Index.html not overwritten

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,24 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const buildPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
   entry: {
     app: './src/index.js',
   },
+  output: {
+    filename: '[name].[hash:20].js',
+    path: buildPath
+},
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: 'body',
+        chunks: ['index'],
+        filename: 'index.html'
+    }),
   ]
 };
